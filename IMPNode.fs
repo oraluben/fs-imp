03bb874b451b75177e4f5d863b0b106318728958
Yichen Yan <or.ange.l.evol@gmail.com>
2017-03-18 22:57:41 +0800

    add label id and program id

--- IMPNode.fs
+++ IMPNode.fs
@@ -14,11 +14,6 @@ type AExp =
     | Int of int
     | Name of AName
     | AExpression of (AExp * A_OP * AExp)
-    member this.ToString : string =
-        match this with
-        | Int(i) -> sprintf "%d" i
-        | Name(n) -> n
-        | AExpression(a1, op, a2) -> sprintf "(%s %s %s)" a1.ToString op.ToString a2.ToString
 
 type B_BIT_OP =
     | AND | OR
@@ -37,12 +32,6 @@ type BExp =
     | Compare of (AExp * B_COMP_OP * AExp)
     | Bitop of (BExp * B_BIT_OP * BExp)
     | Negative of BExp
-    member this.ToString : string =
-        match this with
-        | BBool(b) -> sprintf "%b" b
-        | Compare(a1, op, a2) -> sprintf "(%s %s %s)" a1.ToString op.ToString a2.ToString
-        | Bitop(b1, op, b2) -> sprintf "(%s %s %s)" b1.ToString op.ToString b2.ToString
-        | Negative(b) -> sprintf "(!%s)" b.ToString 
     
 
 type CExp =
@@ -53,15 +42,5 @@ type CExp =
     | Wait of BExp
     | If of (BExp * CExp * CExp)
     | While of (BExp * CExp)
-    member this.ToString : string =
-        match this with
-        | Skip -> "skip"
-        | Co(c1, c2) -> (sprintf "co\n%s\n%s" c1.ToString c2.ToString).Replace("\n", "\n ")
-        | Assign(name, expr) -> sprintf "%s := %s" name expr.ToString
-        | Sequence(c1, c2) -> sprintf "%s\n%s" c1.ToString c2.ToString
-        | Wait(b) -> sprintf "wait(%s)" b.ToString
-        | If(b, c1, c2) -> (sprintf "if %s\n%s\n%s" b.ToString c1.ToString c2.ToString).Replace("\n", "\n ")
-        | While(b, c) -> (sprintf "while %s\n%s" b.ToString c.ToString).Replace("\n", "\n ")
-
-type Program = 
-    { Stmt: CExp }
+    | Program of (int * CExp)
+    | Labeled of (int * CExp)
--- IMPParser.fs
+++ IMPParser.fs
@@ -3,6 +3,25 @@ module IMPParser
 open FParsec
 open IMPNode
 
+type State =
+    {Program: int
+     Label: int}
+    with
+       static member Default = {Program = 0; Label = 0}
+
+let UpdateLabel =
+    updateUserState (fun s -> {s with Label = s.Label + 1})
+let GetLabel : Parser<int, State> =
+    getUserState |>> (fun s -> s.Label)
+let UpdateAndGetLabel =
+    UpdateLabel >>. GetLabel
+let UpdateProgram =
+    updateUserState (fun s -> {s with Program = s.Program + 1})
+let GetProgram : Parser<int, State> =
+    getUserState |>> (fun s -> s.Program)
+let UpdateAndGetProgram =
+    UpdateProgram >>. GetProgram
+
 let tryApply x f =
     match f with
     | Some(_f) -> _f x
@@ -19,8 +38,8 @@ let impAExprOp =
         | '+' -> PLUS
         | '-' | _ -> MINUS
 
-let impAExp, impAExpRef = createParserForwardedToRef<AExp, unit>()
-let impATerm, impATermRef = createParserForwardedToRef<AExp, unit>()
+let impAExp, impAExpRef = createParserForwardedToRef<AExp, State>()
+let impATerm, impATermRef = createParserForwardedToRef<AExp, State>()
 do impATermRef :=
     pipe2   impAFactor
             (opt (pipe2 impATermOp impATerm (fun op a2 -> (fun a1 -> AExpression(a1, op, a2)))))
@@ -44,8 +63,8 @@ let impCompare =
             (fun a1 op a2 -> Compare(a1, op, a2))
 let impBFactor = impBool <|> impCompare |> between spaces spaces
 
-let impBExp, impBExpRef = createParserForwardedToRef<BExp, unit>()
-let impBTerm, impBTermRef = createParserForwardedToRef<BExp, unit>()
+let impBExp, impBExpRef = createParserForwardedToRef<BExp, State>()
+let impBTerm, impBTermRef = createParserForwardedToRef<BExp, State>()
 do impBTermRef :=
     pipe2   impBFactor (opt (pipe2 impBAnd impBExp (fun op a2 -> (fun a1 -> Bitop(a1, op, a2)))))
             tryApply
@@ -55,38 +74,47 @@ do impBExpRef :=
             tryApply
 
 
-let impCExp, impCExpRef = createParserForwardedToRef<CExp, unit>()
-let impCTerm, impCTermRef = createParserForwardedToRef<CExp, unit>()
+let impCExp, impCExpRef = createParserForwardedToRef<CExp, State>()
+let impCTerm, impCTermRef = createParserForwardedToRef<CExp, State>()
+
+let LabelCExp (p, l) = Labeled(l, p)
+let NewProgram (p, l) = Program(l, p)
 
 let impSkip = stringReturn "skip" Skip
 let impWait = pstring "wait" >>. spaces >>. pstring "(" >>. (impBExp |>> Wait) .>> pstring ")"
 let impAssign =
-    pipe3   impName (pstring ":=") impAExp
-            (fun n _ a -> Assign(n, a))
-let impIf =
-    pipe5   impBExp (pstring "then") impCExp (pstring "else") impCExp
-            (fun b _ p1 _ p2 -> If(b, p1, p2))
-    |> between (pstring "if") (pstring "endif")
-let impWhile =
-    pipe3   impBExp (pstring "do") impCExp
-            (fun b _ p -> While(b, p))
-    |> between (pstring "while") (pstring "endwhile")
-let impCo =
-    pipe3   impCExp (pstring "||") impCExp
-            (fun p1 _ p2 -> Co(p1, p2))
-    |> between (pstring "cobegin") (pstring "coend")
-let impSequence =
-    impCTerm .>>.? (pipe2 (pstring ";") impCExp (fun _ p2 -> p2)) |>> Sequence
+    pipe2   (impName .>> (pstring ":=")) impAExp
+            (fun n a -> Assign(n, a))
+    .>>. UpdateAndGetLabel |>> LabelCExp
 
+let impIf =
+    pipe3   ((pstring "if") >>. impBExp)
+            ((pstring "then") >>. impCExp .>>. UpdateAndGetLabel |>> LabelCExp)
+            ((pstring "else") >>. impCExp .>>. UpdateAndGetLabel .>> (pstring "endif") |>> LabelCExp)
+            (fun b p1 p2  -> If(b, p1, p2))
+let impWhile =
+    pipe2   impBExp ((pstring "do") >>. impCExp)
+            (fun b p -> While(b, p))
+    |> between (pstring "while") (pstring "endwhile")
+    .>>. UpdateAndGetLabel |>> LabelCExp
+let impCo =
+    pipe2   (impCExp .>>. UpdateAndGetProgram |>> NewProgram)
+            ((pstring "||") >>. impCExp .>>. UpdateAndGetProgram |>> NewProgram)
+            (fun p1 p2 -> Co(p1, p2))
+    |> between (pstring "cobegin") (pstring "coend")
+    .>>. UpdateAndGetLabel |>> LabelCExp
+let impSequence =
+    impCTerm .>>.? ((pstring ";") >>. impCExp .>>. UpdateAndGetLabel |>> LabelCExp)
+    |>> Sequence
 do impCTermRef :=
     impWhile <|> impIf <|> impCo <|> impSkip <|> impWait <|> impAssign
      |> between spaces spaces
 do impCExpRef :=
     impSequence <|> impCTerm
 
-let Program = impCExp .>> eof
+let Program = impCExp |>> (fun p -> Program(0, p)) .>> eof
 
 let test str =
-    match run Program str with
+    match runParserOnString Program State.Default "" str with
     | Success(result, _, _)   -> printfn "Success: %A" result
     | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
