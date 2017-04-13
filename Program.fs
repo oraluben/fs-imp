e3110a518218bfd83d1549ccb848f157656de819
Yichen Yan <oraluben@outlook.com>
2017-04-13 13:35:44 +0800

    add command line arguments parser

--- Program.fs
+++ Program.fs
@@ -1,29 +1,24 @@
-﻿open FParsec
+﻿open System
+open System.IO
+
+open Argu
+open FParsec
 
 open IMPNode
 open IMPParser
 open IMPLogic
 open IMPKripke
 
-[<EntryPoint>]
-let main argv = 
-    let mutable lexbuf = """
-cobegin
-while True do
-wait(turn=0);
-x:=1;
-turn:=1
-endwhile
-||
-while True do
-wait(turn=1);
-x:=2;
-turn:=0
-endwhile
-coend
-    """
-    lexbuf <- "cobegin cobegin skip || skip coend || skip coend"
+type IMPArguments =
+    | [<MainCommand; ExactlyOnce; Last>] Source of path:string
+with
+    interface IArgParserTemplate with
+        member s.Usage =
+            match s with
+            | Source _ -> "specify an IMP source file."
 
+[<EntryPoint>]
+let main argv =
     let test str =
         match runParserOnString impProgram State.Default "" str with
         | Success(result, state, _) ->
@@ -32,5 +27,11 @@ coend
             printfn "%A" state.SharedVar
             printfn "%A" (Build result).DisjunctionNormalForm
         | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
-    test lexbuf
+
+    let errorHandler = ProcessExiter(colorizer = function ErrorCode.HelpText -> None | _ -> Some ConsoleColor.Red)
+    let argv_results = ArgumentParser.Create<IMPArguments>(errorHandler = errorHandler).Parse argv
+    let imp = match argv_results.TryGetResult <@ Source @> with
+                | Some(s) -> File.ReadAllText(s)
+                | None -> "cobegin cobegin skip || skip coend || skip coend"
+    test imp
     0
